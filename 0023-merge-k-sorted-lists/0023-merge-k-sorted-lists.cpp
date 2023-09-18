e10a974f149919b9d3c151fb266580270b2b6845
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-09-18 15:36:11 +0530

    Time: 16 ms (80.85%), Space: 13.6 MB (40.27%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -18,24 +18,23 @@ class cmp{
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, cmp>q;
+        priority_queue<ListNode* , vector<ListNode*>, cmp>q;
         ListNode* dummy = new ListNode(-1);
         ListNode* tail = dummy;
 
         for(int i=0; i<lists.size(); i++){
-            if(lists[i] != NULL){
+            if(lists[i]!=NULL){
                 q.push(lists[i]);
             }
         }
 
         while(!q.empty()){
             ListNode* node = q.top();
-            // cout<<node->val<<" ";
             q.pop();
             tail->next = node;
             tail = node;
 
-            if(node->next!=NULL) q.push(node->next);
+            if(node->next != NULL) q.push(node->next);
         }
 
         return dummy->next;
