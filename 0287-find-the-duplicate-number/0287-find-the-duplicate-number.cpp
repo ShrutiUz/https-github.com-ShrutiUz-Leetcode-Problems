e59331109179730a35827f08dc79213e9c0b4fcc
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-09-19 10:03:15 +0530

    Time: 68 ms (96.15%), Space: 61.6 MB (28.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,16 +4,17 @@ public:
         int slow = nums[0];
         int fast = nums[0];
 
-        do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        
+        }
 
         slow = nums[0];
 
-        while(slow!=fast){
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
