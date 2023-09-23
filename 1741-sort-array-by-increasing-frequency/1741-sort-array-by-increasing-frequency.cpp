702fd34d33e1f135e71acdd6116d195bf5aca354
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-09-23 11:37:13 +0530

    Time: 3 ms (91.62%), Space: 11.7 MB (10.74%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
+        int n = nums.size();
+
+        unordered_map<int,int>mp;
+
+        for(int i=0; i<n; i++){
             mp[nums[i]]++;
         }
-        priority_queue<pair<int,int>> maxHeap;
-        for(auto it : mp){
-            maxHeap.push({-it.second,it.first});
+
+        priority_queue<pair<int,int>>pq;
+
+        for(auto it: mp){
+            pq.push({-it.second, it.first});
         }
-        vector<int> result;
-        while(!maxHeap.empty()){
-            int x = maxHeap.top().first;
-            for(int i=0;i<abs(x);i++){
-                result.push_back(maxHeap.top().second);
+
+        vector<int>res;
+
+        while(pq.size() != 0){
+            int x = pq.top().first;
+            int y = pq.top().second;
+            pq.pop();
+
+            for(int i=1; i<=abs(x); i++){
+                res.push_back(y);
             }
-            maxHeap.pop();
         }
-        return result;
+
+        // reverse(res.begin(), res.end());
+
+        return res;
     }
 };
