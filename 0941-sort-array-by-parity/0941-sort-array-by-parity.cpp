90bf45a0f57f4ff17636195848df85500f68f59e
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-09-28 08:46:31 +0530

    Time: 3 ms (96.18%), Space: 16.5 MB (36.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -2,28 +2,10 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int n = nums.size();
-        int low = 0;
-        int high = n-1;
 
-        while(low<=high){
-            if(nums[low]%2!=0 && nums[high]%2!=0){
-                //odd and odd 
-                high--;
-            }
-            else if(nums[low]%2==0 && nums[high]%2!=0){
-                //even and odd
-                low++;
-                high--;
-            }
-            else if(nums[low]%2==0 && nums[high]%2==0){
-                //even and even
-                low++;
-            }
-            else if(nums[low]%2!=0 && nums[high]%2==0){
-                //odd and even
-                swap(nums[low], nums[high]);
-                low++;
-                high--;
+        for(int i=0, j=0; j<n; j++){
+            if(nums[j]%2==0){
+                swap(nums[i++], nums[j]);
             }
         }
 
