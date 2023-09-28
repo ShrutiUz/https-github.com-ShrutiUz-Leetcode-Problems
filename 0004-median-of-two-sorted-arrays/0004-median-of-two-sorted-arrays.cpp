e744b43df625e086987a7d2d721e7739b591c45d
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-09-28 23:42:21 +0530

    Time: 26 ms (57.92%), Space: 89.6 MB (58.21%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>vec;
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        int n1 = nums1.size(), n2 = nums2.size();
+        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
 
-        for(int i=0; i<n1; i++){
-            vec.push_back(nums1[i]);
+        int n = n1 + n2;
+        int left = (n1+n2+1)/2;
+
+        int low = 0;
+        int high = n1;
+
+        while(low<=high){
+            int mid1 = (low+high)/2;
+            int mid2 = left - mid1;
+
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if(mid1<n1) r1 = nums1[mid1];
+            if(mid2<n2) r2 = nums2[mid2];
+
+            if(mid1-1>=0) l1 = nums1[mid1-1];
+            if(mid2-1>=0) l2 = nums2[mid2-1];
+
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1) return max(l1, l2);
+                else return ((double)(max(l1,l2) + min(r1,r2)))/2.0; 
+            }
+
+            else if(l1>r2) high = mid1-1;
+            else low = mid1+1;
+            
         }
 
-        for(int i=0; i<n2; i++){
-            vec.push_back(nums2[i]);
-        }
-
-        
-
-        sort(vec.begin(), vec.end());
-
-        for(int i =0; i<vec.size(); i++){
-            cout<<vec[i]<<" ";
-        }
-        cout<<endl;
-
-        int m = vec.size();
-        double median;
-
-        if(m%2== 0){
-            //even size
-            int idx = m/2 - 1;
-            cout<<idx;
-             median = double(vec[idx] + vec[idx+1])/2;
-        }
-        else{
-            int idx = (m+1)/2 - 1;
-            cout<<idx;
-            median = vec[idx];
-        }
-
-        return median;
+        return 0;
     }
 };
