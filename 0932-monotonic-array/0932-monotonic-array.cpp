caa9f9f79734e5e2dfaa3e787c9e9567ad744c70
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-09-29 12:01:40 +0530

    Time: 132 ms (26.24%), Space: 96.8 MB (13.85%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,40 +1,12 @@
 class Solution {
 public:
-
-    bool checkDec(vector<int>&nums){
-        int n = nums.size();
-
-        for(int i=1; i<n; i++){
-            if(nums[i]<=nums[i-1]){
-                continue;
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool checkInc(vector<int>&nums){
-        int n = nums.size();
-
-        for(int i=1; i<n; i++){
-            if(nums[i]>=nums[i-1]){
-                continue;
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
-    }
-
     bool isMonotonic(vector<int>& nums) {
-        int n = nums.size();
-        if(n <= 2) return true;
-        
-        bool inc = checkInc(nums);
-        bool dec = checkDec(nums);
+        bool inc = true, dec = true;
+
+        for(int i=1; i<nums.size(); i++){
+            inc &= (nums[i]>=nums[i-1]);
+            dec &= (nums[i]<=nums[i-1]);
+        }
 
         return inc||dec;
     }
