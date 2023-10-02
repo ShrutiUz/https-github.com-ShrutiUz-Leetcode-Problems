975e0e02e496f1d72077ff9e7519f2247abeadf0
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-10-02 07:34:49 +0530

    Time: 37 ms (33.01%), Space: 13.8 MB (11.45%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -2,21 +2,30 @@ class Solution {
 public:
     bool winnerOfGame(string colors) {
         int n = colors.size();
-        int count_a = 0, count_b = 0;
+        // int count_a = 0, count_b = 0;
+        
+        //using one variable: increment for alice and decrement for bob
+        //return true if the variable is positive at the end
+        //else return false;
+
+        int count = 0;
 
         for(int i=1; i<n-1; i++){
             if(colors[i] == 'A'){
                 if(colors[i-1] == 'A' && colors[i+1] == 'A'){
-                    count_a++;
+                    // count_a++;
+                    count++;
                 }
             }
             else if(colors[i] == 'B'){
                 if(colors[i-1] == 'B' && colors[i+1] == 'B'){
-                    count_b++;
+                    // count_b++;
+                    count--;
                 }
             }
         }
 
-        return count_a>count_b;
+        // return count_a>count_b;
+        return count>0;
     }
 };
