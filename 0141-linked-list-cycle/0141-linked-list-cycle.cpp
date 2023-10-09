29eaa7485c13d3b2d535de441e8d25eaec5a431e
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-10-09 22:27:51 +0530

    Time: 7 ms (88.15%), Space: 8.4 MB (27.62%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,12 +9,31 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL) return head;
-        ListNode *slow = head, *fast = head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast) return true;
+        //create a temp node
+        //as we traverse the list, connect each node to this temp node
+        //and if we find the node->next to be connected to temp already, then return true
+        //else return false
+
+        if(head == NULL){
+            return false;
+        }
+
+        ListNode *temp = new ListNode(-1);
+
+        while(head != NULL){
+            if(head->next == NULL){
+                return false;
+            }
+
+            if(head->next == temp){
+                return true;
+            }
+
+            ListNode* next = head->next;
+            head->next = temp;
+
+            head = next;
+        
         }
 
         return false;
