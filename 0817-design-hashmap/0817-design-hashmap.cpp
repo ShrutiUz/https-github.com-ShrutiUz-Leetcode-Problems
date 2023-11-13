69e1a81f0fda5a696d804e73a163dd2abb39d0c5
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-11-13 23:22:57 +0530

    Time: 594 ms (5.02%), Space: 331.7 MB (5.45%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,24 +1,49 @@
 class MyHashMap {
-public: 
-    vector<int>v;
+public:
+
     int size;
+    vector<list<pair<int,int>>>m;
 
     MyHashMap() {
-      size = 1e6+1;
-      v.resize(size, -1); 
+        size = 1000000;
+        m.resize(size);
+    }
 
+    int hash(int key){
+        return key%size;
+    }
+
+    list<pair<int,int>> :: iterator search(int key){
+        int idx = hash(key);
+        list<pair<int,int>> :: iterator it = m[idx].begin();
+        while(it != m[idx].end()){
+            if(it->first == key){
+                return it;
+            }
+            it++;
+        }
+        return it;
     }
     
     void put(int key, int value) {
-        v[key] = value;
+        int idx = hash(key);
+        remove(key);
+        m[idx].push_back({key, value});
     }
     
     int get(int key) {
-        return v[key];
+        int idx = hash(key);
+        list<pair<int,int>>::iterator it = search(key);
+        if(it == m[idx].end()) return -1;
+        return it->second;
     }
     
     void remove(int key) {
-        v[key] = -1;
+        int idx = hash(key);
+        list<pair<int,int>>::iterator it = search(key);
+        if(it != m[idx].end()){
+            m[idx].erase(it);
+        }
     }
 };
 
