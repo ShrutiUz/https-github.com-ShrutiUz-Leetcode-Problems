a99eb6dd422c2119caaec2e2197176dd9acdc353
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-11-13 13:59:34 +0530

    Time: 40 ms (62.79%), Space: 13.9 MB (35.98%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
+
+    bool isVowel(char c){
+
+        if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+           c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ){
+               return true;
+        }
+        
+        return false;
+    }
+
     string sortVowels(string s) {
+        //create a DS to store the vowels present in the given string
         vector<char>vowels;
+
+        //store the vowels
         for(char c : s){
-            if(string("AEIOUaeiou").find(c) != string::npos){
+            if(isVowel(c)){
                 vowels.push_back(c);
             }
         }
-        sort(vowels.begin(), vowels.end(), greater<char>());
 
-        string result;
+        //sort the vowles
+        sort(vowels.begin(), vowels.end());
+        // E e O e -> E O e e
 
-        for(char c:s){
-            if(string("AEIOUaeiou").find(c) != string::npos){
-                result += vowels.back();
+        //reverse the sorted vector
+        reverse(vowels.begin(), vowels.end());
+        //E O e e -> e e O E
+
+        string t = "";
+
+        for(char c : s){
+            if(isVowel(c)){
+                t += vowels.back();
                 vowels.pop_back();
             }
             else{
-                result += c;
+                t += c;
             }
         }
 
-        return result;
+        return t;
     }
 };
