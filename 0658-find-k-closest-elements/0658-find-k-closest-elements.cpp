6b0714108f2c4101080b1140fbfb238b26f463a4
Shruti Saxena <shrutisaxena2107@gmail.com>
2023-11-13 19:51:36 +0530

    Time: 93 ms (5.16%), Space: 34.5 MB (14.77%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
+        //we can solve this problem by using a maxHeap
+        // this heap will store the (abs(arr[i])-x, arr[i])
+        // the k elements having the smallest abs difference 
+        // will be the k closest
+
+        //define maxHeap of pair<int,int>
         priority_queue<pair<int,int>>pq;
 
-        for(int i=0; i<n; i++){
-            pq.push({abs(x-arr[i]), arr[i]});
+        for(int i=0; i<arr.size(); i++){
+            int diff = abs(arr[i]-x);
+            pq.push({diff, arr[i]});
+
             if(pq.size()>k){
                 pq.pop();
             }
@@ -13,10 +20,10 @@ public:
 
         vector<int>res;
 
-        while(pq.empty() == false){
-            int x = pq.top().second;
+        while(!pq.empty()){
+            int num = pq.top().second;
             pq.pop();
-            res.push_back(x);
+            res.push_back(num);
         }
 
         sort(res.begin(), res.end());
